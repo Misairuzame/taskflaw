05c7e9733b014364151e06811a217fdf4611f3ff
Toni Mobin <toni.mobin+github@gmail.com>
2022-03-11 14:36:51 +0200

    Add registration

--- noteproject/notes/urls.py
+++ noteproject/notes/urls.py
@@ -1,11 +1,12 @@
 from unicodedata import name
 from django.urls import path
-from .views import NoteList, NoteDetail, NoteCreate, NoteUpdate, NoteDelete, CustomLoginView
+from .views import NoteList, NoteDetail, NoteCreate, NoteUpdate, NoteDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', NoteList.as_view(), name='notes'),
     path('note/<int:pk>/', NoteDetail.as_view(), name='note'),
     path('note-create/', NoteCreate.as_view(), name='note-create'),
--- noteproject/notes/views.py
+++ noteproject/notes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from .models import Note
 
@@ -17,6 +18,23 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('notes')
+
+class RegisterPage(FormView):
+    template_name = 'notes/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('notes')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes')
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 class NoteList(LoginRequiredMixin, ListView):
     model = Note
