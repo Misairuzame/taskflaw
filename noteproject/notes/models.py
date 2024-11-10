d8b2087b9f4e28e5c56a9d56d8eb7f930e68ae14
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2024-11-10 16:14:42 +0100

    Removing unnecessary dependencies

--- noteproject/notes/models.py
+++ noteproject/notes/models.py
@@ -1,6 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
-from numpy import blackman
+from django.db import models
+
 
 # Create your models here.
 class Note(models.Model):
